4994ab7de2475df42f091c6da0cc1d72fe6b3529
Igor Redchuk <redchuk@gmail.com>
2016-04-12 22:31:30 +0200

    Setup dependencies

---
+++ app/containers/main.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class Main extends React.Component {
+  render() {
+    return (<div> HELLO! </div>);
+  }
+}
---
+++ app/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Main from './containers/main';
+
+ReactDom.render(
+  <Main />,
+  document.getElementById('app')
+);
