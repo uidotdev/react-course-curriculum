532829f97ec31ad8778d51374182e892e5059201
Igor Redchuk <redchuk@gmail.com>
2016-04-19 23:06:57 +0200

    Add test for response data mapper

--- app/services/response-data-mapper.specs.js
+++ app/services/response-data-mapper.specs.js
@@ -1,13 +1,88 @@
 import { expect } from 'chai';
+import sut from './response-data-mapper';
 
-describe('test environment', () => {
-  let a = false;
+describe('response-data-mapper', () => {
+  let response;
+  let expected;
 
   beforeEach(() => {
-    a = true;
+    response = {
+      city: {
+        name: 'London',
+      },
+      list: [
+        {
+          dt: 1461063600,
+          temp: {
+            day: 282.46,
+            min: 274.31,
+            max: 282.46,
+            night: 274.31,
+            eve: 282.46,
+            morn: 282.46
+          },
+          humidity: 49,
+          weather: [
+            {
+              description: 'clear sky',
+              icon: '01d'
+            }
+          ],
+        },
+        {
+          dt: 1461150000,
+          temp: {
+            day: 284.15,
+            min: 271.88,
+            max: 285.64,
+            night: 276.36,
+            eve: 283.74,
+            morn: 271.88
+          },
+          humidity: 71,
+          weather: [
+            {
+              description: 'clouds',
+              icon: '02d'
+            }
+          ]
+        }
+      ]
+    };
+    
+    expected = {
+      cityName: 'London',
+      days: [
+        {
+          date: new Date(2016, 3, 19, 13).toString(),
+          iconCode: '01d',
+          description: 'clear sky',
+          humidity: 49,
+          temperature: {
+            morning: 9, 
+            day: 9,
+            evening: 9,
+            night: 1
+          }
+        },
+        {
+          date: new Date(2016, 3, 20, 13).toString(),
+          iconCode: '02d',
+          description: 'clouds',
+          humidity: 71,
+          temperature: {
+            morning: -1, 
+            day: 11,
+            evening: 11,
+            night: 3
+          }          
+        }
+      ]
+    }
   });
 
-  it('should work', () => {
-    expect(a).to.equal(true);
+  it('should mapp response correctly', () => {
+    const result = sut.mapForecastResponse(response);
+    expect(result).to.deep.equal(expected);
   })
 });
