d8f080be7bc10e6649d5cac0b6bffe1e2058ea84
David Park <dpark4657@gmail.com>
2017-01-09 23:13:46 -0800

    initializes project with webpack and babel

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+function HelloWorld() {
+  return (
+    <div>Hello World!</div>
+  )
+}
+
+ReactDOM.render(
+  <HelloWorld />,
+  document.getElementById('app')
+)
