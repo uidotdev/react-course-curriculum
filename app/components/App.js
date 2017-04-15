ccbf29b61c32c30492da47885f689c2ee7b16841
Tyler McGinnis <tylermcginnis33@gmail.com>
2017-04-15 17:53:48 -0600

    New changes for step 3 (previously step 4)

--- app/components/App.js
+++ app/components/App.js
@@ -5,8 +5,13 @@ class App extends React.Component {
   render () {
     return (
       <div className='container'>
-        <div>
-          <h1>Main Header</h1>
+        <div className='navbar'>
+          <h1>Clever Title</h1>
+          <ZipCode
+            direction='row'
+            onSubmitZipcode={function(){}}
+            onUpdateZipcode={function(){}}
+            zipcode={123} />
         </div>
         <div className='home-container' style={{backgroundImage: "url('app/images/pattern.svg')"}}>
           <h1 className='header'>Enter a City and State</h1>
