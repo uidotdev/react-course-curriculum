027acf6d4d54ec5656b138bbe2492e72abd03a00
Timothy Joo <timothy.joo@chargify.com>
2018-09-13 16:17:27 -0500

    build out ui for search component

--- app/component/Home.js
+++ app/component/Home.js
@@ -6,8 +6,21 @@ class Home extends React.Component {
   render() {
     return(
       <div>
-        <Header /> 
-        Hi there!
+        <Header />
+        <div className="main">
+          <p>Enter a City & State</p>
+          <input
+            id="location"
+            placeholder="City & State"
+            type="text"
+            autoComplete="off"/>
+          <button
+            className="button"
+            type="submit"
+            >
+            Get Weather
+          </button>
+        </div>
       </div>
     )
   }
