689446e6df35a80a21cf26c444346cd9b0039e2f
Timothy Joo <timothy.joo@chargify.com>
2018-09-14 08:46:34 -0500

    Build header component, implement css styling

--- app/component/Header.js
+++ app/component/Header.js
@@ -5,7 +5,21 @@ class Header extends React.Component {
   render() {
     return(
       <div className="header-style">
-        Main.js Header
+        <p className="header-content-style"> Weather React App! </p>
+        <div className="search-header">
+          <input
+            className="header-input-form"
+            id="location"
+            placeholder="City & State"
+            type="text"
+            autoComplete="off"/>
+          <button
+            className="header-bar-button"
+            type="submit"
+            >
+            Get Weather
+          </button>
+        </div>
       </div>
     )
   }
